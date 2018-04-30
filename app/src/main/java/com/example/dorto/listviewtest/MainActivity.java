9a0547e0068970dde07094b607390fb798fb4134
Moberd-Dortonkon <Moberd-Dortonkon@yandex.ru>
2018-05-01 00:46:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dorto/listviewtest/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.dorto.listviewtest;
+
+import android.database.DataSetObserver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=(ListView)findViewById(R.id.ListView);
+        ArrayList<String>list = new ArrayList<>();
+        list.add("Hello");
+        list.add("Korobot");
+        list.add("You");
+        list.add("Lox");
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+    }
+}
